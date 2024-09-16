0e5b6619017937b9122f31fe7e30568edb4204ce
Milos Tadic b550_Manj <milosgtadic@yahoo.com>
2024-09-17 01:34:59 +0200

    load a single pixel of a 64x64 xpm

--- source/a_init/init_textures.c
+++ source/a_init/init_textures.c
@@ -1,2 +1,92 @@
-#include "../../includes/cub3d.h"
 #include "textures.h"
+
+//////////////////////////////////////////////////////////////////////
+// FAKE
+
+int	close_esc(int keycode, void *param)
+{
+	t_data	*data;
+
+	data = (t_data *)param;
+	if (data->mlx && data->win)
+	{
+		if (keycode == 0xff1b)
+		{
+			mlx_destroy_window(data->mlx, data->win);
+			exit(0);
+		}
+	}
+	return (0);
+}
+
+int	close_x(t_data *data)
+{
+	(void)data;
+	exit(0);
+}
+
+void	fst_mlx_pixel_put(t_data *data, int x, int y, uint32_t color)
+{
+	char	*dst;
+
+	if (x >= 0 && x < SCREEN_W && y >= 0 && y < SCREEN_H)
+	{
+		dst = data->img_buff.addr + (y * data->img_buff.line_length + x * \
+				(data->img_buff.bits_per_pixel / 8));
+		*(uint32_t *)dst = color;
+	}
+}
+
+void	draw_single_column_px(t_data *data, int x, int y, uint32_t pixel)
+{
+	fst_mlx_pixel_put(data, x, y, pixel);
+}
+
+void	draw_columns(t_data *data)
+{
+	int		x;
+	int		y;
+
+	x = -1;
+	while (++x < SCREEN_W)
+	{
+		y = -1;
+		while (++y < SCREEN_H)
+			draw_single_column_px(data, x, y, data->texture[0].pixels[x][y]);
+	}
+}
+
+void	go_mlxing(t_data *data)
+{
+    data->mlx = mlx_init();
+	if (!data->mlx)
+		exit(1);
+	data->win = mlx_new_window(data->mlx, SCREEN_W, SCREEN_H, "cub3d");
+	if (!data->win)
+		exit(2);
+    int size_x;
+    int size_y;
+    data->img_buff.img = mlx_xpm_file_to_image(data->mlx, "./textures/test.xpm", &size_x, &size_y);
+    if (!data->img_buff.img)
+        exit (3);
+	// data->img_buff.img = mlx_new_image(data->mlx, SCREEN_W, SCREEN_H);
+	data->img_buff.addr = mlx_get_data_addr(data->img_buff.img, &data->img_buff.bits_per_pixel, \
+		&data->img_buff.line_length, &data->img_buff.endian);
+    // FAST PUT PIXELS TO IMG BUFFER HERE
+    // draw_columns(data);
+    mlx_put_image_to_window(data->mlx, data->win, data->img_buff.img, 0, 0);
+    mlx_hook(data->win, 2, 1L << 0, close_esc, data);
+	mlx_hook(data->win, 17, 1L << 3, close_x, (void *)data);
+	// Start the MLX event loop
+	mlx_loop(data->mlx);
+}
+
+//////////////////////////////////////////////////////////////////////
+// REAL
+
+int main(void)
+{
+    t_data  data;
+
+    go_mlxing(&data);
+}
--- source/a_init/textures.h
+++ source/a_init/textures.h
@@ -1,9 +1,50 @@
 #ifndef TEXTURES_H
 # define TEXTURES_H
 
+///////////////////////////////////
+// FAKES
+
+# define KO 1
+# define OK 0
+# define TILE_SIZE 64
+# define SCREEN_W 64
+# define SCREEN_H 64
+
+/*
+cc -g init_textures.c -L../../minilibx-linux -lmlx_Linux -L/usr/lib -Iminilibx-linux -lXext -lX11 -lm -lz
+*/
+
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "../../minilibx-linux/mlx.h"
+
+///////////////////////////////////
+// REAL
+
 typedef struct  s_text
 {
     int pixels[TILE_SIZE][TILE_SIZE];
-}   t_text
+}   t_text;
+
+///////////////////////////////////
+// FAKES
+
+typedef struct s_img_buff
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_img_buff;
+
+typedef struct s_data
+{
+	void		*mlx;
+	void		*win;
+	t_img_buff	img_buff;
+    t_text      texture[4];
+}	t_data;
 
 #endif
